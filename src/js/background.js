a97555c08fe70ec38fd54d758522ac4bbb1c3302
typable <contact@typable.dev>
2022-09-14 10:30:26 +0200

    Added icon click to toggle

--- src/js/background.js
+++
@@ -1,1 +0,0 @@
-chrome.action.onClicked.addListener(console.log);
--- src/js/content-script.js
+++
@@ -1,4 +0,0 @@
-const script = document.createElement('script');
-script.type = 'module';
-script.src = chrome.runtime.getURL('./src/js/index.js');
-document.head.appendChild(script);
